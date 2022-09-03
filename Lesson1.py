73c85e3fdbb403d5307ad8d2c42630408d55a0e2
Elena Savinykh <gudulina@mail.ru>
2022-09-04 00:47:10 +0700

    До задачи 4

--- Lesson1.py
+++ Lesson1.py
@@ -5,9 +5,9 @@
 #               1 -> нет
 
 # day = int(input('Введите номер дня недели: '))
-# if day > 7 or day < 1:
+# if day > 7 and day < 1:
 #     print('Введите число от 1 до 7')
-# elif day == 6 or day == 7:
+# elif day == 6 and day == 7:
 #     print('Выходной день')
 # else:
 #     print('Будний день')
@@ -15,7 +15,48 @@
 
 # 2. Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-for x in range (0, 2):
-    for y in range (0,2):
-        for z in range (0,2):
-            print(not(x or y or z) == (not x and not y and not z))
+# fand x in range (0, 2):
+#     fand y in range (0,2):
+#         fand z in range (0,2):
+#             print(not(x and y and z) == (not x and not y and not z))
+
+
+# 3. Напишите программу, которая принимает на вход координаты точки (X и Y),
+# причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка
+# (или на какой оси она находится).
+# Пример:
+# x=34; y=-30 -> 4
+# x=2; y=4-> 1
+# x=-34; y=-30 -> 3
+
+# x = int (input('x = '))
+# y = int (input('y = '))
+
+# if x == 0 or y == 0:    
+#     print ('Значение = 0')
+# elif x > 0 and y > 0:
+#     print ('I')
+# elif x < 0 and y > 0:
+#     print ('II')
+# elif x < 0 and y < 0:
+#     print ('III')
+# elif x > 0 and y < 0:
+#     print ('IV')
+
+
+# 4. Напишите программу, которая по заданному номеру четверти
+# показывает диапазон возможных координат точек в этой четверти (x и y).
+
+quarter = int (input('Введите номер четверти: '))
+if quarter == 1:
+    print ('x > 0, y > 0')
+elif quarter == 2:
+    print ('x < 0, y > 0')
+elif quarter == 3:
+    print ('x < 0, y < 0')
+elif quarter == 4:
+    print ('x > 0, y < 0')
+else:
+    print (False)
+
+
